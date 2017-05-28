bf280fe75d7f12beaa07ad2a4492ebc5ce46c64e
Bucky Roberts <buckyroberts@gmail.com>
2017-05-28 09:09:37 -0700

    Updated UserSerializer to reflect LoginSerializer

--- v1/accounts/serializers/user.py
+++ v1/accounts/serializers/user.py
@@ -6,4 +6,4 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        exclude = ('password',)
+        fields = ('id', 'email', 'first_name', 'last_name')
