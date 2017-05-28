d6cc7a6382d820b2573215e2c9c1966effb2ee32
Bucky <buckyroberts@gmail.com>
2017-05-28 12:01:48 -0700

    Users can be created (generates random password for them)

--- v1/accounts/views/user.py
+++ v1/accounts/views/user.py
@@ -1,7 +1,8 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from v1.accounts.models.user import User
-from v1.accounts.serializers.user import UserSerializer
+from v1.accounts.serializers.user import UserSerializer, UserSerializerCreate
 
 
 # users
@@ -15,3 +16,15 @@ class UserView(APIView):
 
         users = User.objects.all()
         return Response(UserSerializer(users, many=True).data)
+
+    @staticmethod
+    def post(request):
+        """
+        Create user
+        """
+
+        serializer = UserSerializerCreate(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(UserSerializer(serializer.instance).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
