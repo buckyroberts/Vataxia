34aa77278f1e0d1c8d71feacfaf4185f234a91aa
Bucky Roberts <buckyroberts@gmail.com>
2017-05-28 09:15:57 -0700

    Made CreatedModified abstract class for tracking creation and edit date of objects, going to take a shower now

---
+++ v1/general/created_modified.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class CreatedModified(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
