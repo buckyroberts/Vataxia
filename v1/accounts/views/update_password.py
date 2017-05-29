3dbd2d0820a9a4f99baa2b28c031a43b101353be
Bucky <buckyroberts@gmail.com>
2017-05-29 11:12:39 -0700

    Update password for authenticated user

---
+++ v1/accounts/views/update_password.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from v1.utils import constants
+
+
+# update_password
+class UpdatePasswordView(APIView):
+
+    @staticmethod
+    def post(request):
+        """
+        Update password for authenticated user
+        """
+
+        password = request.data.get('password')
+
+        try:
+            validate_password(password)
+            request.user.set_password(password)
+            request.user.save()
+            return Response({constants.SUCCESS: 'Password has been updated'})
+        except KeyError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        except TypeError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        except ValidationError as e:
+            return Response({constants.ERROR: e}, status=status.HTTP_400_BAD_REQUEST)
