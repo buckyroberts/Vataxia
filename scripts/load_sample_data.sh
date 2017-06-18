9bbd9effa12dfa05f76692ba6f1c20fd5f99ba24
Bucky Roberts <buckyroberts@gmail.com>
2017-06-18 00:17:00 -0700

    Adding fixture data

--- scripts/load_sample_data.sh
+++ scripts/load_sample_data.sh
@@ -2,3 +2,5 @@
 
 # Accounts
 python manage.py loaddata v1/accounts/fixtures/user.json
+python manage.py loaddata v1/accounts/fixtures/profile.json
+python manage.py loaddata v1/accounts/fixtures/token.json
