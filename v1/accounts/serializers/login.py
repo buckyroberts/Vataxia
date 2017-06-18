1965339a003ebaae34aa008569d9e4219bf096af
Bucky <buckyroberts@gmail.com>
2017-06-17 19:36:56 -0700

    Adding profile to login serializer for consistency

--- v1/accounts/serializers/login.py
+++ v1/accounts/serializers/login.py
@@ -1,10 +1,22 @@
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
+from v1.accounts.models.profile import Profile
 from v1.accounts.models.user import User
+from .profile import ProfileSerializer
 
 
 class LoginSerializer(serializers.ModelSerializer):
+    profile = serializers.SerializerMethodField()
     token = serializers.SerializerMethodField()
+
+    @staticmethod
+    def get_profile(user):
+        """
+        Get or create profile
+        """
+
+        profile, created = Profile.objects.get_or_create(user=user)
+        return ProfileSerializer(profile, read_only=True).data
 
     @staticmethod
     def get_token(user):
@@ -13,4 +25,4 @@ class LoginSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'email', 'first_name', 'last_name', 'token')
+        fields = ('id', 'email', 'first_name', 'last_name', 'profile', 'token')
