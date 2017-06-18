04ef6755e6f9579c8640c6fd9746e19112976bde
Bucky Roberts <buckyroberts@gmail.com>
2017-06-18 00:21:24 -0700

    Fixing load_sample_data.sh

--- scripts/load_sample_data.sh
+++ scripts/load_sample_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Accounts
-python manage.py loaddata v1/accounts/fixtures/user.json
-python manage.py loaddata v1/accounts/fixtures/profile.json
-python manage.py loaddata v1/accounts/fixtures/token.json
+python3 manage.py loaddata v1/accounts/fixtures/user.json
+python3 manage.py loaddata v1/accounts/fixtures/profile.json
+python3 manage.py loaddata v1/accounts/fixtures/token.json
