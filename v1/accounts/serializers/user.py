27d071340626e6a3bf2bccc2317b0fdf92aa5680
Bucky Roberts <buckyroberts@gmail.com>
2017-06-23 10:23:24 -0700

    Returning user role (administrator, moderator) in user response

--- v1/accounts/serializers/user.py
+++ v1/accounts/serializers/user.py
@@ -2,15 +2,18 @@ from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from v1.accounts.models.profile import Profile
 from v1.accounts.models.user import User
+from v1.utils import constants
+from v1.utils.permissions import is_administrator, is_moderator
 from .profile import ProfileSerializer
 
 
 class UserSerializer(serializers.ModelSerializer):
     profile = serializers.SerializerMethodField()
+    role = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ('id', 'email', 'first_name', 'last_name', 'profile')
+        fields = ('id', 'email', 'first_name', 'last_name', 'profile', 'role')
 
     @staticmethod
     def get_profile(user):
@@ -20,6 +23,17 @@ class UserSerializer(serializers.ModelSerializer):
 
         profile, created = Profile.objects.get_or_create(user=user)
         return ProfileSerializer(profile, read_only=True).data
+
+    @staticmethod
+    def get_role(user):
+        """
+        Get user role
+        """
+
+        if is_administrator(user):
+            return constants.USER_ROLE_ADMINISTRATOR
+        if is_moderator(user):
+            return constants.USER_ROLE_MODERATOR
 
 
 class UserSerializerCreate(UserSerializer):
@@ -49,7 +63,7 @@ class UserSerializerLogin(UserSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'email', 'first_name', 'last_name', 'profile', 'token')
+        fields = ('id', 'email', 'first_name', 'last_name', 'profile', 'role', 'token')
 
 
 class UserSerializerUpdate(serializers.ModelSerializer):
--- v1/utils/constants.py
+++ v1/utils/constants.py
@@ -5,6 +5,10 @@ SUCCESS = 'Success'
 PERMISSION_ADMINISTRATOR_REQUIRED = 'Administrator permissions needed'
 PERMISSION_MODERATOR_REQUIRED = 'Moderator permissions needed'
 
+# User roles
+USER_ROLE_ADMINISTRATOR = 'administrator'
+USER_ROLE_MODERATOR = 'moderator'
+
 # Votes
 VOTE_UP = 1
 VOTE_DOWN = -1
