37f3a3fd082b3d3c4e0898a79f92de35a6d116d9
Bucky Roberts <buckyroberts@gmail.com>
2017-06-24 13:56:58 -0700

    Adding optional image field to post

--- v1/posts/models/post.py
+++ v1/posts/models/post.py
@@ -7,6 +7,7 @@ class Post(CreatedModified):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
     title = models.CharField(max_length=255)
     body = models.TextField()
+    image = models.ImageField(blank=True)
 
     class Meta:
         default_related_name = 'posts'
