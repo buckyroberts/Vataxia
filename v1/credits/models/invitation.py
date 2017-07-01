0f8963c4af19d3b45eff30e7f1a511c8487a5aa0
Bucky Roberts <buckyroberts@gmail.com>
2017-07-01 13:21:22 -0700

    Giving invitation standard pk

--- v1/credits/models/invitation.py
+++ v1/credits/models/invitation.py
@@ -7,7 +7,7 @@ from v1.general.created_modified import CreatedModified
 class Invitation(CreatedModified):
     sender = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='sent_invitations')
     receiver = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, default=None, null=True, related_name='received_invitations')
-    code = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    code = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
 
     class Meta:
         default_related_name = 'invitations'
